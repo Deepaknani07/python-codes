3006a3fe1838a9d05471373bca687fc7d284815a
Deepaknani07 <121151296+Deepaknani07@users.noreply.github.com>
2025-03-11 08:34:13 +0530

    Create swap2.py

---
+++ swap2.py
@@ -0,0 +1,18 @@
+# approach 1
+num1=10
+num2=20
+#apporach 2
+num1=input("enter num1 value:")
+num2=input("enter the num2 value:")
+print("value num1 before swapping :",num1)
+print("value num2 before swapping :",num2)
+#apporach 1
+temp=num1
+num1=num2
+num2=temp
+
+#apporach2
+num1,num2=num2,num1
+
+print("value num1 after swapping:",num1)
+print("value num2 after swapping :",num2)
